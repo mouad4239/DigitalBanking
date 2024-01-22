77a6d110f57e056b3802ec545313d97c15cf129c
= <=>
2024-01-22 14:01:13 +0100

    first local repo

---
+++ src/main/java/ma/fsts/digitbank/DigitBankApplication.java
@@ -0,0 +1,13 @@
+package ma.fsts.digitbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fsts/digitbank/DigitBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsts.digitbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
